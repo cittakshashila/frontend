6e817d4a48ef8c935031e97444929cdb7631f48d
Raviprasath K J <raviprasath320@gmail.com>
2024-01-24 01:32:08 +0530

    changes in the image width

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -11,20 +11,20 @@ const Carousel = () => {
     <>
       <section className="hidden lg:flex flex-col space-y-4">
 
-      <section className="image-container box-1 h-52 rounded-3xl min-w-80  relative z-10 ">
+      <section className="image-container box-1 h-52 rounded-3xl min-w-72  relative z-10 ">
         <img className="w-28 h-44 absolute bottom-0 inset-x-20" src="/mirchisiva.png" alt="First Image"  />
-        <img className=" h-full w-64  absolute top-0 left-0 z-[-1]" src="/mirichisiva-bg.png" alt="Second Image"  />
+        <img className=" h-full w-full  object-cover absolute top-0 left-0 z-[-1]" src="/mirichisiva-bg.png" alt="Second Image"  />
      </section>
 
-     <section className="image-container box-2 h-52 rounded-3xl min-w-80 relative z-10 ">
+     <section className="image-container box-2 h-52 rounded-3xl min-w-72 relative z-10 ">
         <img className="w-32 h-44 absolute bottom-0 left-14" src="/soori.png" alt="First Image" />
-        <img className=" h-full w-64  absolute top-0 left-0 z-[-1]" src="/soori-bg.png" alt="Second Image"  />
+        <img className=" h-full w-full  object-cover  absolute top-0 left-0 z-[-1]" src="/soori-bg.png" alt="Second Image"  />
      </section>
      
        
-     <section className="image-container box-3 h-52 rounded-3xl min-w-80 relative z-10 mt-5">
+     <section className="image-container box-3 h-52 rounded-3xl min-w-72 relative z-10 mt-5">
         <img className="w-32 h-44 absolute bottom-0 left-14 " src="/tbridge.png" alt="First Image" />
-        <img className=" h-full w-64  absolute top-0 left-0 z-[-1]" src="/tbridge-bg.png" alt="Second Image"    />
+        <img className=" h-full w-full object-cover absolute top-0 left-0 z-[-1]" src="/tbridge-bg.png" alt="Second Image"    />
      </section>
 
       </section>
