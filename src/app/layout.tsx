c65048f81975be50b0db4c4d964b185582c8b446
Sak1012 <1012srivatsavas@gmail.com>
2024-01-31 16:18:17 +0530

    feat: SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,29 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { NavBar } from "@/components";
 import { georgia, oranienbaum, passport } from "@/libs/fonts";
+import { title } from "process";
 
 export const metadata: Metadata = {
   title: "CIT - Takshashila 2024",
   description:
     "Takshashila, an event organized In regards to annual cultural event conducted in Chennai Institute Of Technology.",
+  keywords:
+    "TAKSHASHILA, CULTURAL, CIT, CHENNAI, SYMPOSIUM, EVENT, COLLEGE, COMPETITION, INSTITUTE, TECHNOLOGY, DESIGN",
+  themeColor: "#272727",
+  openGraph: {
+    title: "CIT - TAKSHASHILA 2024",
+    description:
+      "Takshashila, an event organized In regards to cultural event conducted in Chennai Institute Of Technology.",
+    type: "website",
+    url: "https://www.cittakshashila.in/",
+    siteName: "CIT - TAKSHASHILA 2024",
+    images: ["/thumbnail.png"],
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@cittakshashila",
+  },
+  robots: "all",
 };
 
 export default function RootLayout({
@@ -16,6 +34,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="revised" content="31/01/2024" />
+        <meta name="thumbnail" content="~/thumbnail.png" />
+      </head>
       <body
         className={`${georgia.variable} ${oranienbaum.variable} ${passport.variable}`}
       >
