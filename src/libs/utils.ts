ad34810bd64b75dcf5ca33d6f2625d9dd6bbd919
Raviprasath K J <raviprasath320@gmail.com>
2024-02-09 18:40:03 +0530

    changed the utils

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -5,6 +5,6 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const isTeaser =false;
+export const isTeaser = process.env.IsTeaser || false;
 export const CLIENT_ID = process.env.CLIENT_ID || "";
 export const CLIENT_SECRET = process.env.CLIENT_SECRET || "";
