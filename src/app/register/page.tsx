7d1f3cccb8886ec9e3a414df25afe0fb98da9992
Raviprasath K J <raviprasath320@gmail.com>
2024-02-11 16:54:05 +0530

    z-index issue resolved in register page :)

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+import {useState} from "react";
 import Image from "next/image";
 import useHighlightScroll from "@/hooks/useHighlightScroll";
 import TakshashilaText from "../../../public/takshashila-text-2024.svg";
@@ -10,6 +11,11 @@ const Register = () => {
   const activeStyle = "border-cream";
   const defaultStyle =
     "no-underline	text-cream border border-transparent transition-all duration-200 p-[3px] rounded";
+
+    const [name,setName] : string = useState("");
+    const [email,setEmail] : string  = useState("");
+    const [college,setCollege] : string = useState("");
+    const [mobile,setMobile] : string = useState("");
 
   return (
     <main className="bg-paint-effect bg-cover bg-center bg-[#272727]  h-screen lg:snap-y lg:snap-mandatory overflow-x-hidden">
@@ -26,33 +32,33 @@ const Register = () => {
                         src={TakshashilaText}
                     />
                </div>
-              <div className="ml-5 text-xl md:text-2xl lg:text-3xl font-passport">
-                    <div className="ml-5 my-[1.5rem] sm:my-[30px] font-passport">
-                        <u >name</u>
+              <div className="ml-5 text-xl md:text-2xl lg:text-3xl font-passport flex flex-col justify-around gap-[2.5rem] flex-start ">
+                    <div className="ml-5 z-50 mt-3 font-passport flex">
+                      <input type="text" placeholder="name" name="username" required className="bg-transparent" onChange = {event =>{setName(event.target.value)}} />
                       </div>   
-                      <div className="ml-5 my-[1.5rem] sm:my-[30px] font-passport"> 
-                        from  <u>college</u> to chennai institute of technology
+                      <div className="ml-5 z-50 font-passport flex flex-wrap flex-row"> 
+                        from  <input type="text" placeholder="college" required name="college" className="pl-5 bg-transparent flex" onChange = {event=>{setCollege(event.target.value)}}/>to chennai institute of technology
                       </div>
-                      <div className="ml-5 my-[1.5rem] sm:my-[30px]  font-passport">
-                        <u>mobile no</u>
+                      <div className="ml-5 z-50  font-passport flex">
+                         <input type="number" placeholder="mobile no" required name="mobile no" className="bg-transparent" onChange = {event =>{setMobile(event.target.value)}}/>
                       </div>
-                      <div className="ml-5 mt-[1rem] mb-[0.5rem] sm:my-[30px] font-passport">
-                        <u>email</u>
+                      <div className="ml-5 z-50  font-passport flex">
+                        <input type="email" placeholder="email" required name="email" className="bg-transparent" onChange={(event)=>{setEmail(event.target.value)}}/>
                       </div>
                       <div className="flex ml-5 flex-row font-bold  pr-5 align-center flex-wrap gap-2 md:gap-6 text-[20px] sm:text-xl md:text-2xl lg:text-3xl w-[90%] justify-center mb-[20px] items-center font-passport">
                            <div className="z-30">GENERAL</div>
                            <div className="z-30">WORKSHOP</div>
                            <div className="z-30">PRO-SHOW</div>
-                           <button className="rounded-lg p-2 bg-[#1E1E1E] z-[50] text-[#FFFFFF]">Register</button>
+                           <button className="rounded-lg p-2 bg-[#1E1E1E] z-50 text-[#FFFFFF]">Register</button>
                       </div>
                </div>
                 <Image 
-                    className="absolute top-0  sm:right-[10%] w-fit h-[140%]  opacity-[70%]"
+                    className="absolute top-0  sm:right-[10%] w-fit h-[140%]  opacity-[60%]"
                     src={TakshashilaLogo}
                     alt="Takshashila-logo"
                     />
                 <Image 
-                    className="absolute  h-[120%] w-full bottom-[1%] sm:h-[140%] md:h-[130%] lg:h-[100%] opacity-[70%]"
+                    className="absolute z-10 h-[120%] w-[100%] bottom-[1%] sm:h-[140%] md:h-[130%] lg:h-[100%] opacity-[70%]"
                     src={WorldMap}
                     alt="World-Map"
                     />
@@ -61,8 +67,6 @@ const Register = () => {
                     src={Year}
                     alt="World-Map"
                     />
-                    
-            
         </div>
 
       </section>
