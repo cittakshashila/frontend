b4b0cde129cacee24bb1b20817712aebefa108ee
Mukesh Kannan <mukeshkannan311@gmail.com>
2024-02-16 11:55:21 +0530

    feat: event page type fix

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,14 +1,14 @@
-"use client"
-import { EventList } from "@/libs/types";
+"use client";
+import { EVENT } from "@/libs/types";
 import React, { useState } from "react";
 
 interface Props {
-  events: EventList;
+  events: EVENT;
 }
 
 const Card = ({ events }: Props) => {
   const cardStyle: React.CSSProperties = {
-    backgroundImage: `url(${events.event_img})`,
+    backgroundImage: `url(https://img.etimg.com/thumb/width-1200,height-1200,imgsize-84711,resizemode-75,msid-51251228/news/sports/pro-kabaddi-league-is-helping-international-competitors-catch-up.jpg)`,
     backgroundSize: "cover",
     backgroundPosition: "center",
     color: "white",
@@ -29,22 +29,18 @@ const Card = ({ events }: Props) => {
       <div className="p-5 h-[90%]  w-[100%]">
         {isHovered && (
           <div className="mt-5">
-            <p>{events.event_type}|{events.event_date}</p>
+            <p>
+              {events.details.type}|{events.details.date}
+            </p>
           </div>
         )}
-        {isHovered && (
-          <div className="text-2xl mt-12">
-            {events.event_tag}
-          </div>
-        )}
+        {isHovered && <div className="text-2xl mt-12">{events.tagline}</div>}
         {isHovered && (
           <button className="mt-5 sm:mt-20 p-3  text-[#FFF] rounded-lg bg-gray-200 bg-opacity-30">
             Learn More
           </button>
         )}
-        <div className="text">
-          {events.event_name}
-        </div>
+        <div className="text">{events.title}</div>
       </div>
     </div>
   );
--- src/libs/types.ts
+++ src/libs/types.ts
@@ -1,27 +1,3 @@
-export type EventList = {
-  event_name: string;
-  event_tag: string;
-  event_details: string;
-  event_type: string;
-  event_date: string;
-  event_img: string;
-  event_hover_img:string;
-};
-
-export type EventDescriptor = {
-  event_name: string;
-  event_tag: string;
-  event_details: string;
-  event_type: string;
-  event_date: string;
-  event_img: string;
-  event_fees: string;
-  event_time: string;
-  contact_name: string;
-  contact_number: string;
-  contact_email: string;
-};
-
 type CONTACT = {
   incharge: string;
   email: string;
