0f42578e5c2b86c3f9739ff96f582f0a885de19d
RahulNavneeth <rahulmnavneeth@gmail.com>
2024-02-17 22:54:38 +0530

    FEAT: FAST FETCH

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -2,200 +2,98 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
 import Card from "@/components/Card";
-import { EVENT, FOLDER_TYPE } from "@/libs/types";
-import { PARSE } from "@/libs/utils";
+import { FOLDER_TYPE } from "@/libs/types";
 import { NavBar } from "@/components";
 import { IconLoader } from "@tabler/icons-react";
+import { PARSE } from "@/libs/utils";
 
 export default function Events() {
+    type Type = "All" | "TECHNICAL" | "NON-TECHNICAL" | "WORKSHOP" | "PRO SHOW" | "ONLINE EVENT";
+    type infoType = { name: string; type: Type };
     const [filter, setFilter] = useState<string>("All");
-    const [queue, setQueue] = useState<Array<FOLDER_TYPE>>([]);
-    const [EventList, setEventList] = useState<Array<EVENT>>([]);
-
-    const LIMIT = 10;
+    const [queue, setQueue] = useState<Record<string, infoType> | null>(null);
 
     useEffect(() => {
         const fetchFolders = async () => {
             const res = await fetch("/api/events/folder");
-            const data = (await res.json()) as {
-                payload: { tree: { items: Array<FOLDER_TYPE> } };
-            };
-            setQueue(
-                data.payload.tree.items.filter(
-                    (item) => item.contentType === "directory",
-                ),
-            );
+            const data = (await res.json()) as { payload: { blob: { rawLines: Array<string> } } };
+            console.log(data);
+            setQueue(PARSE(data.payload.blob.rawLines) as Record<string, infoType>);
+            console.log(queue);
         };
-        if (queue.length === 0) {
+        if (!queue) {
             fetchFolders();
         }
     }, []);
-
-    const fetchEvent = async (name: string) => {
-        const res = await fetch(`/api/events/${name}`);
-        const data = await res.json();
-        return data.payload.blob.rawLines;
-    };
-    const processEvents = async () => {
-        const tempEventList = [...EventList];
-        let updatedQueue = [...queue];
-        for (let i = 0; i < Math.min(LIMIT, queue.length); i++) {
-            const item = updatedQueue.shift();
-            if (item) {
-                const data = await fetchEvent(item.name);
-                if (!tempEventList.find((e) => e.title === PARSE(data).title)) {
-                    tempEventList.push(PARSE(data) as EVENT);
-                }
-            }
-        }
-        setEventList([...tempEventList]);
-        setQueue([...updatedQueue]);
-    };
-
-    useEffect(() => {
-        setTimeout(() => {
-            if (queue.length > 0) processEvents();
-        }, 100);
-    }, [queue]);
-
-<<<<<<< HEAD
-    const filteredEvents = EventList.filter(
-        (item) => filter === "All" || item.details.type === filter,
-    );
-
+    const filteredEvents = Object.entries(queue || {}).filter(([_, value]) => filter === "All" || value.type === filter);
     return (
         <div className="bg-paint-effect bg-cover bg-center bg-[#272727] h-screen lg:snap-y lg:snap-mandatory overflow-x-hidden">
             <div className=" bg-fixed bg-cover bg-center m-2">
                 <NavBar />
-                <div className="relative top-32 flex flex-row flex-wrap justify-center">
-                    <div className="sticky  w-full flex justify-center items-center top-10 z-[999]">
-                        <ul className="text-cream px-10 text-md flex justify-center items-center space-x-4 show cursor-pointer">
-                            <a
-                                className={filter === "All" ? "underline text-[#f0f0f0]" : ""}
-                                onClick={() => setFilter("All")}
-                            >
-                                All
-                            </a>
-                            <a
-                                className={
-                                    filter === "TECHNICAL" ? "underline text-[#f0f0f0]" : ""
-                                }
-                                onClick={() => setFilter("TECHNICAL")}
-                            >
-                                Technical
-                            </a>
-                            <a
-                                className={
-                                    filter === "NON-TECHNICAL" ? "underline text-[#f0f0f0]" : ""
-                                }
-                                onClick={() => setFilter("NON-TECHNICAL")}
-                            >
-                                Non-Technical
-                            </a>
-                            <a
-                                className={
-                                    filter === "WORKSHOP" ? "underline text-[#f0f0f0]" : ""
-                                }
-                                onClick={() => setFilter("WORKSHOP")}
-                            >
-                                Workshops
-                            </a>
-                            <a
-                                className={
-                                    filter === "PRO SHOW" ? "underline text-[#f0f0f0]" : ""
-                                }
-                                onClick={() => setFilter("PRO SHOW")}
-                            >
-                                Pro Show
-                            </a>
-                            <a
-                                className={
-                                    filter === "ONLINE EVENT" ? "underline text-[#f0f0f0]" : ""
-                                }
-                                onClick={() => setFilter("ONLINE EVENT")}
-                            >
-                                Online Event
-                            </a>
-                        </ul>
-                    </div>
 
-                    {filteredEvents.map((item, index: number) => (
-                        <Card events={item} key={index} />
-                    ))}
-                </div>
+                {!queue ? (
+                    <div className="min-h-screen flex items-center justify-center">
+                        <IconLoader size={72} className="animate-spin text-white" />
+                    </div>
+                ) : (
+                    <div className="relative top-32 flex flex-row flex-wrap justify-center">
+                        <div className="sticky w-full flex justify-center items-center xl:top-36 top-3 px-5  md:z-[999] z-40 ">
+                            <ul className="text-cream px-10 text-md flex justify-center flex-wrap items-center space-x-4 rounded-xl backdrop-blur py-4 cursor-pointer">
+                                <a
+                                    className={filter === "All" ? "underline text-[#f0f0f0]" : ""}
+                                    onClick={() => setFilter("All")}
+                                >
+                                    All
+                                </a>
+                                <a
+                                    className={
+                                        filter === "TECHNICAL" ? "underline text-[#f0f0f0]" : ""
+                                    }
+                                    onClick={() => setFilter("TECHNICAL")}
+                                >
+                                    Technical
+                                </a>
+                                <a
+                                    className={
+                                        filter === "NON-TECHNICAL" ? "underline text-[#f0f0f0]" : ""
+                                    }
+                                    onClick={() => setFilter("NON-TECHNICAL")}
+                                >
+                                    Non-Technical
+                                </a>
+                                <a
+                                    className={
+                                        filter === "WORKSHOP" ? "underline text-[#f0f0f0]" : ""
+                                    }
+                                    onClick={() => setFilter("WORKSHOP")}
+                                >
+                                    Workshops
+                                </a>
+                                <a
+                                    className={
+                                        filter === "PRO SHOW" ? "underline text-[#f0f0f0]" : ""
+                                    }
+                                    onClick={() => setFilter("PRO SHOW")}
+                                >
+                                    Pro Show
+                                </a>
+                                <a
+                                    className={
+                                        filter === "ONLINE EVENT" ? "underline text-[#f0f0f0]" : ""
+                                    }
+                                    onClick={() => setFilter("ONLINE EVENT")}
+                                >
+                                    Online Event
+                                </a>
+                            </ul>
+                        </div>
+
+                        {filteredEvents.map(([key, value], index: number) => (
+                            <Card id={key} name={value.name} key={index} />
+                        ))}
+                    </div>
+                )}
             </div>
         </div>
     );
-=======
-  return (
-    <div className="bg-paint-effect bg-cover bg-center bg-[#272727] h-screen lg:snap-y lg:snap-mandatory overflow-x-hidden">
-      <div className=" bg-fixed bg-cover bg-center m-2">
-        <NavBar />
-
-        {EventList.length === 0 ? (
-          <div className="min-h-screen flex items-center justify-center">
-            <IconLoader size={72} className="animate-spin text-white" />
-          </div>
-        ) : (
-          <div className="relative top-32 flex flex-row flex-wrap justify-center">
-            <div className="sticky w-full flex justify-center items-center xl:top-36 top-3 px-5  md:z-[999] z-40 ">
-              <ul className="text-cream px-10 text-md flex justify-center flex-wrap items-center space-x-4 rounded-xl backdrop-blur py-4 cursor-pointer">
-                <a
-                  className={filter === "All" ? "underline text-[#f0f0f0]" : ""}
-                  onClick={() => setFilter("All")}
-                >
-                  All
-                </a>
-                <a
-                  className={
-                    filter === "TECHNICAL" ? "underline text-[#f0f0f0]" : ""
-                  }
-                  onClick={() => setFilter("TECHNICAL")}
-                >
-                  Technical
-                </a>
-                <a
-                  className={
-                    filter === "NON-TECHNICAL" ? "underline text-[#f0f0f0]" : ""
-                  }
-                  onClick={() => setFilter("NON-TECHNICAL")}
-                >
-                  Non-Technical
-                </a>
-                <a
-                  className={
-                    filter === "WORKSHOP" ? "underline text-[#f0f0f0]" : ""
-                  }
-                  onClick={() => setFilter("WORKSHOP")}
-                >
-                  Workshops
-                </a>
-                <a
-                  className={
-                    filter === "PRO SHOW" ? "underline text-[#f0f0f0]" : ""
-                  }
-                  onClick={() => setFilter("PRO SHOW")}
-                >
-                  Pro Show
-                </a>
-                <a
-                  className={
-                    filter === "ONLINE EVENT" ? "underline text-[#f0f0f0]" : ""
-                  }
-                  onClick={() => setFilter("ONLINE EVENT")}
-                >
-                  Online Event
-                </a>
-              </ul>
-            </div>
-
-            {filteredEvents.map((item, index: number) => (
-              <Card events={item} key={index} />
-            ))}
-          </div>
-        )}
-      </div>
-    </div>
-  );
->>>>>>> upstream/main
 }
