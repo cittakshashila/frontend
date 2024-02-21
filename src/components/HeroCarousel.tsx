aed800dd38ff7a33fe8b5a3187e152929fdc4e70
Sak1012 <1012srivatsavas@gmail.com>
2024-02-21 22:01:10 +0530

    feat Changes in Events Page 🍩

--- src/components/HeroCarousel.tsx
+++ src/components/HeroCarousel.tsx
@@ -29,6 +29,7 @@ import "swiper/css";
 import "swiper/css/pagination";
 import Image from "next/image";
 import { Autoplay } from "swiper/modules";
+import { motion } from "framer-motion";
 
 export default function HeroCarousel() {
   const textCarousel = [
@@ -43,265 +44,284 @@ export default function HeroCarousel() {
   return (
     <>
       <section className="h-screen flex space-x-0  md:space-x-[10px] absolute w-[250px] md:w-[350px] xl:w-[500px] left-0 right-0 mx-auto z-0">
-        <Swiper
-          loop={true}
-          allowTouchMove={false}
-          slidesPerView={"auto"}
-          direction="vertical"
-          centeredSlides={true}
-          autoplay={{ delay: 3000 }}
-          modules={[Autoplay]}
+        <motion.section
           className="h-full w-full"
+          initial={{ y: -1000 }}
+          animate={{ y: 0 }}
+          transition={{ duration: 1 }}
         >
-          <SwiperSlide className="relative bg-grey">
-            <Image
-              className="h-screen -z-20 bg-[#272727]"
-              alt="bg"
-              src={IMG1}
-              layout="fill"
-              objectFit="cover"
-              objectPosition="center"
-              loading="lazy"
-            />
-          </SwiperSlide>
+          <Swiper
+            loop={true}
+            allowTouchMove={false}
+            slidesPerView={"auto"}
+            direction="vertical"
+            centeredSlides={true}
+            autoplay={{ delay: 3000 }}
+            modules={[Autoplay]}
+            className="h-full w-full"
+          >
+            <SwiperSlide className="relative bg-grey">
+              <Image
+                className="h-screen -z-20 bg-[#272727]"
+                alt="bg"
+                src={IMG1}
+                layout="fill"
+                objectFit="cover"
+                objectPosition="center"
+                loading="lazy"
+              />
+            </SwiperSlide>
 
-          <SwiperSlide className="relative bg-grey">
-            <Image
-              className="h-screen -z-20 bg-[#272727]"
-              alt="bg"
-              src={IMG2}
-              layout="fill"
-              objectFit="cover"
-              objectPosition="center"
-              loading="lazy"
-            />
-          </SwiperSlide>
+            <SwiperSlide className="relative bg-grey">
+              <Image
+                className="h-screen -z-20 bg-[#272727]"
+                alt="bg"
+                src={IMG2}
+                layout="fill"
+                objectFit="cover"
+                objectPosition="center"
+                loading="lazy"
+              />
+            </SwiperSlide>
 
-          <SwiperSlide className="relative bg-grey">
-            <Image
-              className="h-screen -z-20 bg-[#272727]"
-              alt="bg"
-              src={IMG3}
-              layout="fill"
-              objectFit="cover"
-              objectPosition="center"
-              loading="lazy"
-            />
-          </SwiperSlide>
+            <SwiperSlide className="relative bg-grey">
+              <Image
+                className="h-screen -z-20 bg-[#272727]"
+                alt="bg"
+                src={IMG3}
+                layout="fill"
+                objectFit="cover"
+                objectPosition="center"
+                loading="lazy"
+              />
+            </SwiperSlide>
 
-          <SwiperSlide className="relative bg-grey">
-            <Image
-              className="h-screen -z-20 bg-[#272727]"
-              alt="bg"
-              src={IMG4}
-              layout="fill"
-              objectFit="cover"
-              objectPosition="center"
-              loading="lazy"
-            />
-          </SwiperSlide>
+            <SwiperSlide className="relative bg-grey">
+              <Image
+                className="h-screen -z-20 bg-[#272727]"
+                alt="bg"
+                src={IMG4}
+                layout="fill"
+                objectFit="cover"
+                objectPosition="center"
+                loading="lazy"
+              />
+            </SwiperSlide>
 
-          <SwiperSlide className="relative bg-grey">
-            <Image
-              className="h-screen -z-20 bg-[#272727]"
-              alt="bg"
-              src={IMG5}
-              layout="fill"
-              objectFit="cover"
-              objectPosition="center"
-              loading="lazy"
-            />
-          </SwiperSlide>
+            <SwiperSlide className="relative bg-grey">
+              <Image
+                className="h-screen -z-20 bg-[#272727]"
+                alt="bg"
+                src={IMG5}
+                layout="fill"
+                objectFit="cover"
+                objectPosition="center"
+                loading="lazy"
+              />
+            </SwiperSlide>
 
-          <SwiperSlide className="relative bg-grey">
-            <Image
-              className="h-screen -z-20 bg-[#272727]"
-              alt="bg"
-              src={IMG6}
-              layout="fill"
-              objectFit="cover"
-              objectPosition="center"
-              loading="lazy"
-            />
-          </SwiperSlide>
-        </Swiper>
-
-        <Swiper
-          slidesPerView={"auto"}
-          allowTouchMove={false}
-          direction="vertical"
+            <SwiperSlide className="relative bg-grey">
+              <Image
+                className="h-screen -z-20 bg-[#272727]"
+                alt="bg"
+                src={IMG6}
+                layout="fill"
+                objectFit="cover"
+                objectPosition="center"
+                loading="lazy"
+              />
+            </SwiperSlide>
+          </Swiper>
+        </motion.section>
+        <motion.section
           className="h-full w-full"
-          loop={true}
-          centeredSlides={true}
-          autoplay={{ delay: 3000, reverseDirection: true }}
-          modules={[Autoplay]}
+          initial={{ y: 1000 }}
+          animate={{ y: 0 }}
+          transition={{ duration: 1 }}
         >
-          <SwiperSlide className="relative bg-grey">
-            <Image
-              className="h-screen -z-20 bg-[#272727]"
-              alt="bg"
-              src={IMG7}
-              layout="fill"
-              objectFit="cover"
-              objectPosition="center"
-              loading="lazy"
-            />
-          </SwiperSlide>
+          <Swiper
+            slidesPerView={"auto"}
+            allowTouchMove={false}
+            direction="vertical"
+            className="h-full w-full"
+            loop={true}
+            centeredSlides={true}
+            autoplay={{ delay: 3000, reverseDirection: true }}
+            modules={[Autoplay]}
+          >
+            <SwiperSlide className="relative bg-grey">
+              <Image
+                className="h-screen -z-20 bg-[#272727]"
+                alt="bg"
+                src={IMG7}
+                layout="fill"
+                objectFit="cover"
+                objectPosition="center"
+                loading="lazy"
+              />
+            </SwiperSlide>
 
-          <SwiperSlide className="relative bg-grey">
-            <Image
-              className="h-screen -z-20 bg-[#272727]"
-              alt="bg"
-              src={IMG8}
-              layout="fill"
-              objectFit="cover"
-              objectPosition="center"
-              loading="lazy"
-            />
-          </SwiperSlide>
+            <SwiperSlide className="relative bg-grey">
+              <Image
+                className="h-screen -z-20 bg-[#272727]"
+                alt="bg"
+                src={IMG8}
+                layout="fill"
+                objectFit="cover"
+                objectPosition="center"
+                loading="lazy"
+              />
+            </SwiperSlide>
 
-          <SwiperSlide className="relative bg-grey">
-            <Image
-              className="h-screen -z-20 bg-[#272727]"
-              alt="bg"
-              src={IMG8}
-              layout="fill"
-              objectFit="cover"
-              objectPosition="center"
-              loading="lazy"
-            />
-          </SwiperSlide>
+            <SwiperSlide className="relative bg-grey">
+              <Image
+                className="h-screen -z-20 bg-[#272727]"
+                alt="bg"
+                src={IMG8}
+                layout="fill"
+                objectFit="cover"
+                objectPosition="center"
+                loading="lazy"
+              />
+            </SwiperSlide>
 
-          <SwiperSlide className="relative bg-grey">
-            <Image
-              className="h-screen -z-20 bg-[#272727]"
-              alt="bg"
-              src={IMG9}
-              layout="fill"
-              objectFit="cover"
-              objectPosition="center"
-              loading="lazy"
-            />
-          </SwiperSlide>
+            <SwiperSlide className="relative bg-grey">
+              <Image
+                className="h-screen -z-20 bg-[#272727]"
+                alt="bg"
+                src={IMG9}
+                layout="fill"
+                objectFit="cover"
+                objectPosition="center"
+                loading="lazy"
+              />
+            </SwiperSlide>
 
-          <SwiperSlide className="relative bg-grey">
-            <Image
-              className="h-screen -z-20 bg-[#272727]"
-              alt="bg"
-              src={IMG10}
-              layout="fill"
-              objectFit="cover"
-              objectPosition="center"
-              loading="lazy"
-            />
-          </SwiperSlide>
+            <SwiperSlide className="relative bg-grey">
+              <Image
+                className="h-screen -z-20 bg-[#272727]"
+                alt="bg"
+                src={IMG10}
+                layout="fill"
+                objectFit="cover"
+                objectPosition="center"
+                loading="lazy"
+              />
+            </SwiperSlide>
 
-          <SwiperSlide className="relative bg-grey">
-            <Image
-              className="h-screen -z-20 bg-[#272727]"
-              alt="bg"
-              src={IMG11}
-              layout="fill"
-              objectFit="cover"
-              objectPosition="center"
-              loading="lazy"
-            />
-          </SwiperSlide>
+            <SwiperSlide className="relative bg-grey">
+              <Image
+                className="h-screen -z-20 bg-[#272727]"
+                alt="bg"
+                src={IMG11}
+                layout="fill"
+                objectFit="cover"
+                objectPosition="center"
+                loading="lazy"
+              />
+            </SwiperSlide>
 
-          <SwiperSlide className="relative bg-grey">
-            <Image
-              className="h-screen -z-20 bg-[#272727]"
-              alt="bg"
-              src={IMG12}
-              layout="fill"
-              objectFit="cover"
-              objectPosition="center"
-              loading="lazy"
-            />
-          </SwiperSlide>
-          <SwiperSlide className="relative bg-grey">
-            <Image
-              className="h-screen -z-20 bg-[#272727]"
-              alt="bg"
-              src={IMG13}
-              layout="fill"
-              objectFit="cover"
-              objectPosition="center"
-              loading="lazy"
-            />
-          </SwiperSlide>
-        </Swiper>
-
-        <Swiper
-          allowTouchMove={false}
-          loop={true}
-          slidesPerView={"auto"}
-          direction="vertical"
+            <SwiperSlide className="relative bg-grey">
+              <Image
+                className="h-screen -z-20 bg-[#272727]"
+                alt="bg"
+                src={IMG12}
+                layout="fill"
+                objectFit="cover"
+                objectPosition="center"
+                loading="lazy"
+              />
+            </SwiperSlide>
+            <SwiperSlide className="relative bg-grey">
+              <Image
+                className="h-screen -z-20 bg-[#272727]"
+                alt="bg"
+                src={IMG13}
+                layout="fill"
+                objectFit="cover"
+                objectPosition="center"
+                loading="lazy"
+              />
+            </SwiperSlide>
+          </Swiper>
+        </motion.section>
+        <motion.section
           className="h-full w-full"
-          centeredSlides={true}
-          autoplay={{ delay: 3000 }}
-          modules={[Autoplay]}
+          initial={{ y: -1000 }}
+          animate={{ y: 0 }}
+          transition={{ duration: 1 }}
         >
-          <SwiperSlide className="relative bg-grey">
-            <Image
-              className="h-screen -z-20 bg-[#272727]"
-              alt="bg"
-              src={IMG14}
-              layout="fill"
-              objectFit="cover"
-              objectPosition="center"
-              loading="lazy"
-            />
-          </SwiperSlide>
+          <Swiper
+            allowTouchMove={false}
+            loop={true}
+            slidesPerView={"auto"}
+            direction="vertical"
+            className="h-full w-full"
+            centeredSlides={true}
+            autoplay={{ delay: 3000 }}
+            modules={[Autoplay]}
+          >
+            <SwiperSlide className="relative bg-grey">
+              <Image
+                className="h-screen -z-20 bg-[#272727]"
+                alt="bg"
+                src={IMG14}
+                layout="fill"
+                objectFit="cover"
+                objectPosition="center"
+                loading="lazy"
+              />
+            </SwiperSlide>
 
-          <SwiperSlide className="relative bg-grey">
-            <Image
-              className="h-screen -z-20 bg-[#272727]"
-              alt="bg"
-              src={IMG15}
-              layout="fill"
-              objectFit="cover"
-              objectPosition="center"
-              loading="lazy"
-            />
-          </SwiperSlide>
+            <SwiperSlide className="relative bg-grey">
+              <Image
+                className="h-screen -z-20 bg-[#272727]"
+                alt="bg"
+                src={IMG15}
+                layout="fill"
+                objectFit="cover"
+                objectPosition="center"
+                loading="lazy"
+              />
+            </SwiperSlide>
 
-          <SwiperSlide className="relative bg-grey">
-            <Image
-              className="h-screen -z-20 bg-[#272727]"
-              alt="bg"
-              src={IMG16}
-              layout="fill"
-              objectFit="cover"
-              objectPosition="center"
-              loading="lazy"
-            />
-          </SwiperSlide>
+            <SwiperSlide className="relative bg-grey">
+              <Image
+                className="h-screen -z-20 bg-[#272727]"
+                alt="bg"
+                src={IMG16}
+                layout="fill"
+                objectFit="cover"
+                objectPosition="center"
+                loading="lazy"
+              />
+            </SwiperSlide>
 
-          <SwiperSlide className="relative bg-grey">
-            <Image
-              className="h-screen -z-20 bg-[#272727]"
-              alt="bg"
-              src={IMG17}
-              layout="fill"
-              objectFit="cover"
-              objectPosition="center"
-              loading="lazy"
-            />
-          </SwiperSlide>
+            <SwiperSlide className="relative bg-grey">
+              <Image
+                className="h-screen -z-20 bg-[#272727]"
+                alt="bg"
+                src={IMG17}
+                layout="fill"
+                objectFit="cover"
+                objectPosition="center"
+                loading="lazy"
+              />
+            </SwiperSlide>
 
-          <SwiperSlide className="relative bg-grey">
-            <Image
-              className="h-screen -z-20 bg-[#272727]"
-              alt="bg"
-              src={IMG18}
-              layout="fill"
-              objectFit="cover"
-              objectPosition="center"
-              loading="lazy"
-            />
-          </SwiperSlide>
-        </Swiper>
+            <SwiperSlide className="relative bg-grey">
+              <Image
+                className="h-screen -z-20 bg-[#272727]"
+                alt="bg"
+                src={IMG18}
+                layout="fill"
+                objectFit="cover"
+                objectPosition="center"
+                loading="lazy"
+              />
+            </SwiperSlide>
+          </Swiper>
+        </motion.section>
       </section>
 
       <section className="absolute left-0 right-0 mx-auto z-10 w-fit bottom-28">
--- src/components/HomeEventComponent.tsx
+++ src/components/HomeEventComponent.tsx
@@ -1,33 +1,83 @@
 import Image, { StaticImageData } from "next/image";
 import Link from "next/link";
+import { cn } from "@/libs/utils";
+import { useState } from "react";
+import { motion } from "framer-motion";
 
 interface prop {
   title: string;
   description: string;
-  background: StaticImageData;
+  image1: StaticImageData;
+  image2: StaticImageData;
+  image3: StaticImageData;
+  type?: "left" | "right";
 }
 
 export default function HomeEventComponent({
   title,
   description,
-  background,
+  image1,
+  image2,
+  image3,
+  type = "left",
 }: prop) {
+  const [isHovered, setIsHovered] = useState(false);
   return (
-    <section className="h-full xl:h-auto relative flex flex-col items-center justify-between w-full rounded-xl py-5">
-      <Image
-        className="rounded-l -z-10"
-        src={background}
-        layout="fill"
-        objectFit="cover"
-        objectPosition="center"
-        alt={title}
-      />
-      <h1 className="transition-all text-white text-center py-2 text-4xl w-full backdrop-blur bg-opacity-50 bg-cream shadow">
+    <motion.section
+      className={cn(
+        "lg:flex backdrop-blur-sm w-full h-full min-w-56 rounded-lg overflow-clip",
+        type === "left" && "flex-row-reverse",
+      )}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <h1
+        className={cn(
+          "flex invisible items-center text-cream text-xl md:visible lg:text-[85px] w-1/2 px-5",
+          type === "right" ? "text-right" : "text-left",
+        )}
+      >
         {title}
       </h1>
-      <Link href={"/events"} className="bg-white w-3/4 py-2 font-bold text-xl text-center font-oranienbaum rounded-md  shadow">
-        Learn More
-      </Link>
-    </section>
+      <section className="lg:relative absolute top-0 flex flex-grow  lg:w-0">
+        <section className="absolute top-0 flex -space-x-20">
+          <motion.section
+            initial={{ x: 0, y: 0 }}
+            animate={{ x: isHovered ? -250 : 0, y: isHovered ? 250 : 0 }}
+            transition={{ duration: 0.5, type: "tween", delay: 0.1 }}
+          >
+            <Image src={image1} alt="image3" />{" "}
+          </motion.section>
+          <motion.section
+            initial={{ x: 0, y: 0 }}
+            animate={{ x: isHovered ? 250 : 0, y: isHovered ? -250 : 0 }}
+            transition={{ duration: 0.5, type: "tween", delay: 0.1 }}
+          >
+            <Image src={image2} alt="image3" />
+          </motion.section>
+          <motion.section
+            initial={{ x: 0, y: 0 }}
+            animate={{ x: isHovered ? -250 : 0, y: isHovered ? 250 : 0 }}
+            transition={{ duration: 0.5, type: "tween", delay: 0.1 }}
+          >
+            <Image src={image3} alt="image3" />
+          </motion.section>
+        </section>
+        <motion.p
+          className={cn(
+            "flex items-center text-cream text-[12px] md:text-lg px-2",
+            type === "left" ? "text-right" : "text-left",
+          )}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: isHovered ? 1 : 0 }}
+          transition={{ duration: 0.5, delay: 0.2 }}
+        >
+          {description}
+        </motion.p>
+        <h1 className="absolute text-cream sm:text-[50px] md:hidden lg:text-[85px] px-5">
+          {title}
+        </h1>
+      </section>
+    </motion.section>
   );
 }
