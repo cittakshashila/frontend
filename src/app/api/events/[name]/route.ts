d7e62af097db64e671d915de222cd877acc70f99
Mukesh Kannan <mukeshkannan311@gmail.com>
2024-02-23 15:42:31 +0530

    fix: Event page fetching issue fixed

--- src/app/api/events/[name]/route.ts
+++ src/app/api/events/[name]/route.ts
@@ -1,8 +1,12 @@
 import { FOLDER_URL, INFO_URL } from "@/libs/utils";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest, { params }: { params: { name: string } }) {
-    const { name } = params;
-    const json = await fetch(`https://github.com/cittakshashila/EVENTS-DATA-24/blob/master/events/${name}/info.json`).then(res => res.json());
-    return NextResponse.json(json);
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { name: string } },
+) {
+  const { name } = params;
+  const json = await fetch(INFO_URL(name || "")).then((res) => res.json());
+
+  return NextResponse.json(json);
 }
--- src/app/api/events/folder/route.ts
+++ src/app/api/events/folder/route.ts
@@ -2,6 +2,7 @@ import { FOLDER_INFO_URL, FOLDER_URL } from "@/libs/utils";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(_: NextRequest) {
-    const json = await fetch(`https://github.com/cittakshashila/EVENTS-DATA-24/tree/master/events`).then(res => res.json());
-    return NextResponse.json(json);
+  const json = await fetch(FOLDER_INFO_URL).then((res) => res.json());
+
+  return NextResponse.json(json);
 }
